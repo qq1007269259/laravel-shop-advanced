976ed5fc1add9d0a980356e3f05591699d3ab65d
vagrant <vagrant@homestead>
2019-05-20 02:58:30 +0000

    前期分期付款列表页

--- routes/web.php
+++ routes/web.php
@@ -44,6 +44,7 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
 
     Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
     Route::post('crowdfunding_orders', 'OrdersController@crowdfunding')->name('crowdfunding_orders.store');
+    Route::get('installments', 'InstallmentsController@index')->name('installments.index');
     Route::post('payment/{order}/installment', 'PaymentController@payByInstallment')->name('payment.installment');
 });
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
