c524f471ee76a4a23a9e6036e7987211b8213766
vagrant <vagrant@homestead>
2019-05-27 06:51:27 +0000

    初始化秒杀模型

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,11 +7,13 @@ use Illuminate\Support\Str;
 
 class Product extends Model
 {
+    const TYPE_SECKILL = 'seckill';
     const TYPE_NORMAL = 'normal';
     const TYPE_CROWDFUNDING = 'crowdfunding';
     public static $typeMap = [
         self::TYPE_NORMAL  => '普通商品',
         self::TYPE_CROWDFUNDING => '众筹商品',
+        self::TYPE_SECKILL => '秒杀商品',
     ];
 
     protected $fillable = [
@@ -105,5 +107,10 @@ class Product extends Model
     public function scopeByIds($query, $ids)
     {
         return $query->whereIn('id', $ids)->orderByRaw(sprintf("FIND_IN_SET(id, '%s')", join(',', $ids)));
+    }
+
+    public function seckill()
+    {
+        return $this->hasOne(SeckillProduct::class);
     }
 }
